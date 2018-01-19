27742ec39f52a91217b35dfefd9edbbc14a55024
Daniel Estévez <daniel.ef@gmail.com>
2018-01-19 15:21:01 -0600

    Controls styles & texts

--- src/cljs/lienzo/components/diagram.cljs
+++ src/cljs/lienzo/components/diagram.cljs
@@ -9,8 +9,10 @@
             :current {:id "id"}}
  :draw {}}
 
-(defn edge [state-atm & {:keys [id class x1 y1 x2 y2 label]
-                            :as   args}]
+(defn edge 
+  "Draw an edge from x1 y1 to x2 y2 and put label as text inside"
+  [state-atm & {:keys [id class x1 y1 x2 y2 label]
+                :as   args}]
   (fn [state-atm & {:keys [id class x1 y1 x2 y2 label]
                     :as   args}]
     (let [h (- y2 y1)                          ;; height        
@@ -25,13 +27,13 @@
           _ (.log js/console (str  "m>> " m " g(rad)> " (Math/atan m) " g(deg)> " (/  (* (Math/atan m) 180) Math/PI)))]
       [:g
        [:defs
-        [:pattern {:id id-pat 
+        [:pattern {:id id-pat  ;; pattern with arrows
                    :patternUnits "userSpaceOnUse" 
                    :x 0 
                    :y (+ y1 5) ;; tricky background offset
                    :width 14 
                    :height 30}
-         [:rect {:x 0 :y 0 :width 14 :height 40 :style {:fill "rgba(128,128,128,0.5)"}}]
+         [:rect {:x 0 :y 0 :width 14 :height 40 :style {:fill "rgba(128,128,128,0.5)"}}] 
          [:line {:x1 5 :y1 5 :x2 13 :y2 13 :style {:stroke "#eee" :stroke-width "1"} }]
          [:line {:x1 13 :y1 13 :x2 5 :y2 20 :style {:stroke "rgba(255,255,255,0.5)" :stroke-width "1"} }]]]
        [:rect {:x x1
@@ -42,24 +44,29 @@
                :style {:fill (str "url(#" id-pat ")")}}]
        (let [x  (+ x1 (/ b 2) (if (< h 0) 13 -13))
              y  (+ y1 (/ h 2) (if (< b 0) -13 13))
-             idc (str "connector-" id-pat)]         
+             idc (str "connector-" id-pat)
+             cx  (- x (* (count label) 4))]         
          [:g
           [:defs
            [:filter {:id idc}
             [:feGaussianBlur {:in "SourceGraphic" :stdDeviation 1}]
             ]]
           [:text {:x x :y y :fill "white" :text-anchor "middle" :alignment-baseline "central" :font-family "Verdana" :font-size "9" :filter (str "url(#" idc ")") :transform (str  "rotate(" (+ d 0) " " x "," y ")")} label]
-          [:text {:x x :y y :fill "black" :text-anchor "middle" :alignment-baseline "central" :font-family "Verdana" :font-size "9" :transform (str  "rotate(" (+ d 0) " " x "," y ")")} label]])])))
+          [:text {:x x :y y :fill "black" :text-anchor "middle" :alignment-baseline "central" :font-family "Verdana" :font-size "9" :transform (str  "rotate(" (+ d 0) " " x "," y ")")} label]
+          [:circle {:cx cx :cy y :r 5 :transform (str  "rotate(" (+ d 0) " " x "," y ")")}]])])))
 
 (defn on 
-  "Takes ev and register "
+  "Fired when MouseDown occurs in a node"
   [ev state-atm id]
   (let [ne (.-nativeEvent ev)
         page-x (.-pageX ne)
         page-y (.-pageY ne)]
     
+    ;; Takes our node id and replace :selected values
     (swap! state-atm update-in [:selected] assoc :drag {:id id :last [page-x page-y]}
                                                  :current {:id id})
+
+    ;; When a edge is drawed
     (if-let [line (get-in @state-atm [:draw :line])]
       (let [prev-id (get-in @state-atm [:draw :id])
             [p-offset-x  p-offset-y] (get-in @state-atm [:nodes prev-id :position] [0 0])
@@ -70,24 +77,24 @@
                      :x2 (+ offset-x 30)
                      :y2 (+ offset-y 30)]]
         (swap! state-atm update-in [:connections] conj {:from prev-id :to id :label "added"})
-        
-        (swap! state-atm dissoc :draw)))
+        (swap! state-atm dissoc :draw)))))
 
-    ;(.log js/console (str "on " @state-atm))
-    ))
-
-(defn off [state-atm]
+(defn off 
+  "Fired when MouseUp occurs in a node"
+  [state-atm]
   ;(swap! state-atm update-in [:selected :drag] #(get-in @state-atm [:selected :previous]))
-  (swap! state-atm update-in [:selected] dissoc :drag)
-  ;(.log js/console (str "off " @state-atm))
-  )
+  
+  ;; Stop to dragging node
+  (swap! state-atm update-in [:selected] dissoc :drag))
 
-(defn move [ev state-atm]
+(defn move 
+  "Fired when MouseMove occurs over svg"
+  [ev state-atm]
   (let [ne (.-nativeEvent ev)        
         page-x (.-pageX ne)
         page-y (.-pageY ne)]
-                                        
-    ;; dragging 
+    
+    ;; When dragging a node 
     (if-let [drag (get-in @state-atm [:selected :drag])] 
       (let [{:keys [id last]} drag
             [last-x last-y] last
@@ -96,34 +103,32 @@
                                                   (- (+ offset-y page-y) last-y)])
         (swap! state-atm update-in [:selected] assoc :drag {:id id :last [page-x page-y]})))
 
-    ;; drawing
+    ;; When drawing a line
     (if-let [line (get-in @state-atm [:draw :line])]
       (let [id (get-in @state-atm [:draw :id])
             [offset-x offset-y] (get-in @state-atm [:nodes id :position] [0 0])]
         (swap! state-atm update-in [:draw] assoc :line [edge state-atm
-                                                        :x1 (+ offset-x 30) 
+                                                        :x1 (+ offset-x 30)
                                                         :y1 (+ offset-y 30)
                                                         :x2 page-x
-                                                        :y2 page-y
-                                                        #_{
-                                                         :style {:stroke "rgb(0,0,255)"
-                                                                       :stroke-width 2}}])))
-    ))
+                                                        :y2 page-y])))))
 
-(defn click [ev state-atm]
+(defn click 
+  "Fired when onClick occurs over svg"
+  [ev state-atm]
   (let [ne (-> ev .-nativeEvent)
         source-tag (-> ne .-srcElement .-nodeName)
         page-x (.-pageX ne)
-        page-y (.-pageY ne)
-        ;_ (.log js/console ne)
-        ]
+        page-y (.-pageY ne)]
+
+    ;; When click is over empty space
     (cond (= source-tag "svg")
           (do (swap! state-atm update-in [:selected] dissoc :current)
-              (swap! state-atm dissoc :draw))
-          ;(= source-tag "circle")
-          )))
+              (swap! state-atm dissoc :draw)))))
 
-(defn draw-on [ev state-atm id]
+(defn draw-on 
+  "Fired when OnMouseDown in Connect"
+  [ev state-atm id]
   (.stopPropagation ev) 
   (.preventDefault ev)
   (let [ne (.-nativeEvent ev)
@@ -146,6 +151,31 @@
     ;(.log js/console (str "draw-on " @state-atm))
     ))
 
+(defn moveover [ev state-atm id]
+  (.stopPropagation ev) 
+  (.preventDefault ev)
+  
+  (let [ne (-> ev .-nativeEvent)
+        class-tag (-> ne .-srcElement .-className .-baseVal)]
+    (cond (= class-tag "circle")
+          (swap! state-atm update-in [:title-hover] assoc :type id)
+          (= class-tag "l-circle")
+          (swap! state-atm update-in [:node-hover] assoc :type id))))
+
+(defn moveout [ev state-atm id]
+  (.stopPropagation ev) 
+  (.preventDefault ev)
+  (swap! state-atm dissoc :title-hover))
+
+(defn delete [ev state-atm id]
+  
+  (swap! state-atm update-in [:nodes] dissoc id)
+  (swap! state-atm update-in [:selected] dissoc :drag)
+  (swap! state-atm update-in [:selected] dissoc :current)
+  (swap! state-atm dissoc :draw)
+  
+  )
+
 (defn node
   [state-atm & {:keys [id class x y s]
                 :as   args}]
@@ -154,26 +184,49 @@
     (fn [state-atm & {:keys [id class x y s]
                       :or {id id class class}
                       :as args}]
+      (.log js/console (str @state-atm))
       (let [;_ (.log js/console (str {:id id}))
             [offset-x offset-y] (get-in @state-atm [:nodes id :position] [0 0])
             ;_ (.log js/console (str {:id id :position [offset-x offset-y]}))
-            current? (= id (get-in @state-atm [:selected :current :id]))            
-            radius (* 2  15.91549430918954)
-            ]
+            current? (= id (get-in @state-atm [:selected :current :id]))
+            hover? (= id (get-in @state-atm [:node-hover :type]))
+            type (get-in @state-atm [:title-hover :type])
+            radius (* 2  15.91549430918954)]
         [:g (assoc args 
                       :id id :class class 
                       :onMouseDown #(on % state-atm id)
                       :onMouseUp #(off state-atm)
-                      ;:onMouseOver #(info % state-atm id)
+                      :onMouseOver #(moveover % state-atm id)
                       ;:onMouseLeave #(off state-atm)
                       :transform (str "translate(" offset-x "," offset-y ")")
                       :class (if current? "grp selected" "grp"))
          
          [:circle {:cx 30 :cy  30 :r (* 2 radius) :class "circle-panel" :shape-rendering "optimizeQuality"}]
-         [:circle {:cx 30 :cy  30 :r radius :class "circle" :shape-rendering "optimizeQuality" :onMouseDown #(draw-on % state-atm id)}
-          [:title "connect"]]
-         [:circle {:cx 30 :cy  30 :r radius :class "circle" :shape-rendering "optimizeQuality"}]
-         [:circle {:cx 30 :cy  30 :r radius :class "circle" :shape-rendering "optimizeQuality"}]
+         
+         [:circle {:cx 30 :cy  30 :r radius :class "circle" :shape-rendering "optimizeQuality" 
+                   :onMouseDown #(draw-on % state-atm id) 
+                   :onMouseOver #(moveover % state-atm "connect") 
+                   :onMouseOut #(moveout % state-atm nil) }]
+         [:text {:class (if (and hover? (= type "connect")) "title selected" "title") 
+                 :x 82 :y 18 :fill "black" :font-family "Verdana" :font-weight "lighter" :font-size "8" 
+                 :transform "rotate(-18 80,18)"} "connect"]
+         
+         [:circle {:cx 30 :cy  30 :r radius :class "circle" :shape-rendering "optimizeQuality"
+                   ;:onMouseDown #(draw-on % state-atm id) 
+                   :onMouseOver #(moveover % state-atm "edit") 
+                   :onMouseOut #(moveout % state-atm nil) }]
+         [:text {:class (if (and hover? (= type "edit")) "title selected" "title") 
+                 :x 63 :y -12 :fill "black" :font-family "Verdana" :font-weight "lighter" :font-size "8" 
+                 :transform "rotate(-54 63,-12)"} "edit"]
+         
+         [:circle {:cx 30 :cy  30 :r radius :class "circle" :shape-rendering "optimizeQuality"
+                   :onMouseDown #(delete % state-atm id)
+                   :onMouseOver #(moveover % state-atm "delete") 
+                   :onMouseOut #(moveout % state-atm nil) }]
+         [:text {:class (if (and hover? (= type "delete")) "title selected" "title") 
+                 :x 32 :y -24 :fill "black" :font-family "Verdana" :font-weight "lighter" :font-size "8" 
+                 :transform "rotate(-90 32,-24)"} "delete"]
+
          [:circle {:cx 30 :cy  30 :r radius :class "circle" :shape-rendering "optimizeQuality"}]
          [:circle {:cx 30 :cy  30 :r radius :class "circle" :shape-rendering "optimizeQuality"}]
          [:circle {:cx 30 :cy  30 :r radius :class "l-circle" :shape-rendering "optimizeQuality"}]
@@ -190,19 +243,7 @@
                     :font-family "Verdana" :font-size "9" :filter (str "url(#" idc ")") :lengthAdjust "spacingAndGlyphs" } id]
             [:text {:x (+ -2 (- 60 (min 60 (* 5 (count id))))) :y 78 
                     :textLength (min 60 (* 4.5 (count id))) 
-                    :font-family "Verdana" :font-size "9" :lengthAdjust "spacingAndGlyphs"} id]])
-         
-         ;[:circle {:cx 30 :cy  30 :r radius :class "circle-panel" :shape-rendering "optimizeQuality"}]
-         ;[:circle {:cx 30 :cy  30 :r 8 :class "circle" :shape-rendering "optimizeQuality" :transform "translate(22.28,-22.28)"}]
-         ;[:circle {:cx 30 :cy  30 :r 6 :class "circle" :shape-rendering "optimizeQuality" :transform "translate(-22.28,-22.28)"}]
-         ;[:circle {:cx 30 :cy  30 :r 8 :class "circle" :shape-rendering "optimizeQuality" :transform "translate(0,-31.28)"}]
-         ;[:circle {:cx 30 :cy  30 :r 6 :class "circle" :shape-rendering "optimizeQuality" :transform "translate(-31.28,0)"}]
-         ;[:circle {:cx 30 :cy  30 :r 6 :class "circle" :shape-rendering "optimizeQuality" :transform "translate(0,31.28)"}]
-         ;[:circle {:cx 30 :cy  30 :r 8 :class "circle" :shape-rendering "optimizeQuality" :transform "translate(31.28,0)"}]
-         ;[:circle {:cx 30 :cy  30 :r 6 :class "circle" :shape-rendering "optimizeQuality" :transform "translate(22.28,22.28)"}]
-         ;[:circle {:cx 30 :cy  30 :r 6 :class "circle" :shape-rendering "optimizeQuality" :transform "translate(-22.28,22.28)"}]
-
-         ]))))
+                    :font-family "Verdana" :font-size "9" :lengthAdjust "spacingAndGlyphs"} id]])]))))
 
 
 
@@ -215,12 +256,9 @@
                    (fn [r {:keys [label from to]}]
                      (let [[x1 y1] (get-in @state-atm [:nodes from :position])
                            [x2 y2] (get-in @state-atm [:nodes to :position])]
-                       (conj r [edge state-atm :x1 (+  x1 30) :y1 (+ y1 30) :x2 (+ x2 30) :y2 (+ y2 30) :label label])
-                       ;(.log js/console (str {:from [x1 y1] :to [x2 y2]}))
-                       ))
+                       (conj r [edge state-atm :x1 (+  x1 30) :y1 (+ y1 30) :x2 (+ x2 30) :y2 (+ y2 30) :label label])))
                    []
-                   (:connections @state-atm))
-            ;_ (.log js/console (str (:nodes @state-atm)))
+                   (:connections @state-atm))                                        
             set (reduce-kv
                  (fn [r k v]
                    ;(.log js/console (str "reduce: " [k v]))
