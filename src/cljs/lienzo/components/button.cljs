c973323bb49eba5a438a8add3936d2fba969d324
Daniel Estevez <daniel.ef@gmail.com>
2018-05-22 09:21:58 -0500

    Add button component improved

--- src/cljs/lienzo/components/button.cljs
+++ src/cljs/lienzo/components/button.cljs
@@ -1,17 +1,32 @@
 (ns lienzo.components.button
-  (:require [lienzo.utils.js :as util-js] 
+  (:require [goog.dom.classlist :as gclasses]
+            [lienzo.utils.js :as util-js] 
             [reagent.core :as r]))
-
 
 (defn button 
   ([]
-   [button {} "button"])
+   [button {} "Button"])
   ([text]
-   [button {} text])
+   (if (map? text)
+     [button text nil]
+     [button {} text]))
   ([args text]
    (r/create-class {:reagent-render (fn [args text]
-                                      [:button.lnz args text])
+                                      (if-let [icon (:icon args)]
+                                        [:button.lnz args [:span [icon] (if text [:span text])]]
+                                        [:button.lnz args text]))
                     :component-did-mount (fn [component]
                                            (let [element (r/dom-node component)]
                                              (-> element
-                                                 (util-js/event-add-remove "active" "mousedown" "mouseup"))))})))
+                                                 (util-js/add-event-listener "keydown" (fn [e]
+                                                                                         (let [key-code (-> e .-keyCode)]
+                                                                                           (if (or (= key-code 13) (= key-code 32))
+                                                                                             (gclasses/add element (util-js/type->class "active"))))))
+                                                 (util-js/add-event-listener "keyup" (fn [e]
+                                                                                       (let [key-code (-> e .-keyCode)]
+                                                                                         (if (or (= key-code 13) (= key-code 32))
+                                                                                           (gclasses/remove element (util-js/type->class "active"))))))
+                                                 (util-js/event-add-remove "active" "mousedown" "mouseup")
+                                                 (util-js/event-add-remove "active" "touchstart" "touchend")
+                                                 (util-js/event-add-remove "over" "mouseover" "mouseout")
+                                                 (util-js/event-add-remove "over" "focusin" "focusout"))))})))
