4be59e6fc49917528f35ca2cbe51e280edb7bc49
Daniel Estevez <daniel.ef@gmail.com>
2018-06-03 00:41:31 -0500

    Adding radio button tests & css

--- test/cljs/lienzo/tests.cljs
+++ test/cljs/lienzo/tests.cljs
@@ -2,4 +2,5 @@
   (:require [lienzo.diagram-test]
             [lienzo.select-test]
             [lienzo.button-test]
-            [lienzo.checkbox-test]))
+            [lienzo.checkbox-test]
+            [lienzo.radio-test]))
