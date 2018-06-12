c16acaa77e054b065b3d22aff84b811134d197c7
Daniel Estevez <daniel.ef@gmail.com>
2018-06-11 20:40:36 -0500

    Update textfield tests

--- test/cljs/lienzo/textfield_test.cljs
+++ test/cljs/lienzo/textfield_test.cljs
@@ -20,11 +20,9 @@
   ```
   produces in hiccup:
   ```
-  [:label.lnz {:for \"random-uuid\"}                ;; Label wrapper
-    [:i.lnz {:tab-index 0}]                       ;; Stylized Pseudo INPUT
-    [:input {:id \"random-uuid\" :type \"textfield\"}] ;; Real INPUT 
-    \"\"]
-     
+  [:label.lnz {:for \"random-uuid\"}                           ;; Label wrapper
+    [:span.name \"\"]                                          ;; Field name
+    [:span.field [:input {:type \"text\" :id \"random-uuid\"}]]] ;; Real INPUT wrapped by SPAN    
   "
   [textfield])
 
@@ -32,16 +30,17 @@
   (let [label (-> js/document
                   (.getElementsByClassName "lnz")
                   (aget 0))
-        pseudo-input (-> label .-firstChild)
-        input (-> pseudo-input .-nextSibling)]
+        name  (-> label .-firstChild)
+        field (-> name .-nextSibling)
+        input (-> field .-firstChild)]
     (testing "Wrapper is a `<label>`?"
       (is (= (-> label .-tagName) "LABEL")))
     (testing "First child of label is an `<i>`?"
-      (is (= (-> pseudo-input .-tagName) "I")))
+      (is (= (-> name .-tagName) "SPAN")))
     (testing "Real input is an `<input>`?" 
-      (is (= (-> input .-tagName) "INPUT")))
+      (is (= (-> field .-tagName) "SPAN")))
     (testing "And input is of type `textfield`" 
-      (is (= (-> input .-type) "textfield")))
+      (is (= (-> input .-type) "text")))
     (testing "`<label>` contains '' as textContent?"
       (is (= (-> label .-textContent) "")))))
 
@@ -57,18 +56,19 @@
 (deftest test-textfield-arity-one-text
   (let [label (-> js/document
                   (.getElementsByClassName "lnz")
-                  (aget 2))
-        pseudo-input (-> label .-firstChild)
-        input (-> pseudo-input .-nextSibling)]
+                  (aget 1))
+        name  (-> label .-firstChild)
+        field (-> name .-nextSibling)
+        input (-> field .-firstChild)]
     (testing "Wrapper is a `<label>`?"
       (is (= (-> label .-tagName) "LABEL")))
     (testing "First child of label is an `<i>`?"
-      (is (= (-> pseudo-input .-tagName) "I")))
+      (is (= (-> name .-tagName) "SPAN")))
     (testing "Real input is an `<input>`?" 
-      (is (= (-> input .-tagName) "INPUT")))
+      (is (= (-> field .-tagName) "SPAN")))
     (testing "And input is of type `textfield`" 
-      (is (= (-> input .-type) "textfield")))
-    (testing "`<label>` contains 'Textfield Arity One' as textContent?"
+      (is (= (-> input .-type) "text")))
+    (testing "`<label>` contains '' as textContent?"
       (is (= (-> label .-textContent) "Textfield Arity One")))))
 
 (defcard-rg card-textfield-arity-one-attrs
@@ -83,21 +83,20 @@
 (deftest test-textfield-arity-one-attrs
   (let [label (-> js/document
                   (.getElementsByClassName "lnz")
-                  (aget 4))
-        pseudo-input (-> label .-firstChild)
-        input (-> pseudo-input .-nextSibling)]
+                  (aget 2))
+        name  (-> label .-firstChild)
+        field (-> name .-nextSibling)
+        input (-> field .-firstChild)]
     (testing "Wrapper is a `<label>`?"
       (is (= (-> label .-tagName) "LABEL")))
     (testing "First child of label is an `<i>`?"
-      (is (= (-> pseudo-input .-tagName) "I")))
+      (is (= (-> name .-tagName) "SPAN")))
     (testing "Real input is an `<input>`?" 
-      (is (= (-> input .-tagName) "INPUT")))
+      (is (= (-> field .-tagName) "SPAN")))
     (testing "And input is of type `textfield`" 
-      (is (= (-> input .-type) "textfield")))
+      (is (= (-> input .-type) "text")))
     (testing "`<label>` contains '' as textContent?"
-      (is (= (-> label .-textContent) "")))
-    (testing "`<input>` are checked?"
-      (is (-> input .-checked)))))
+      (is (= (-> label .-textContent) "")))))
 
 (defcard-rg card-textfield-arity-two
   "
@@ -112,49 +111,78 @@
 (deftest test-textfield-arity-two
   (let [label (-> js/document
                   (.getElementsByClassName "lnz")
-                  (aget 6))
-        pseudo-input (-> label .-firstChild)
-        input (-> pseudo-input .-nextSibling)]
+                  (aget 3))
+        name  (-> label .-firstChild)
+        field (-> name .-nextSibling)
+        input (-> field .-firstChild)]
     (testing "Wrapper is a `<label>`?"
       (is (= (-> label .-tagName) "LABEL")))
     (testing "First child of label is an `<i>`?"
-      (is (= (-> pseudo-input .-tagName) "I")))
+      (is (= (-> name .-tagName) "SPAN")))
     (testing "Real input is an `<input>`?" 
-      (is (= (-> input .-tagName) "INPUT")))
+      (is (= (-> field .-tagName) "SPAN")))
     (testing "And input is of type `textfield`" 
-      (is (= (-> input .-type) "textfield")))
-    (testing "`<label>` contains 'Textfield Arity Two' as textContent?"
+      (is (= (-> input .-type) "text")))
+    (testing "`<label>` contains '' as textContent?"
       (is (= (-> label .-textContent) "Textfield Arity Two")))))
 
 (defcard-rg card-textfield-disabled
   "
   ## Testing a disable textfield, see cursor and animation!
   ```
-  [textfield {:disabled \"disabled\"} \"Textfield Disabled\"]
+  [textfield {:disabled true :value \"Foo\"} \"Textfield Disabled\"]
   ```
   "
-  [:div
-   [textfield {:disabled "disabled" :value "Foo"} "Textfield Disabled"]
-   [textfield {:read-only true :value "Bar"} "Textfield ReadOnly"]])
+  [textfield {:disabled true :value "Foo"} "Textfield Disabled"])
 
 (deftest test-textfield-disabled
   (let [label (-> js/document
                   (.getElementsByClassName "lnz")
-                  (aget 8))
-        pseudo-input (-> label .-firstChild)
-        input (-> pseudo-input .-nextSibling)]
+                  (aget 4))
+        name  (-> label .-firstChild)
+        field (-> name .-nextSibling)
+        input (-> field .-firstChild)]
     (testing "Wrapper is a `<label>`?"
       (is (= (-> label .-tagName) "LABEL")))
     (testing "First child of label is an `<i>`?"
-      (is (= (-> pseudo-input .-tagName) "I")))
+      (is (= (-> name .-tagName) "SPAN")))
     (testing "Real input is an `<input>`?" 
-      (is (= (-> input .-tagName) "INPUT")))
-    (testing "And input is of type `textfield`" 
-      (is (= (-> input .-type) "textfield")))
-    (testing "`<label>` contains 'Textfield Disabled' as textContent?"
-      (is (= (-> label .-textContent) "Textfield Disabled")))
-    (testing "`<input>` are disabled?"
-      (is (-> input .-disabled)))))
+      (is (= (-> field .-tagName) "SPAN")))
+    (testing "And input is of type `text`" 
+      (is (= (-> input .-type) "text")))
+    (testing "And input hast value `Foo`" 
+      (is (= (-> input .-value) "Foo")))
+    (testing "`<label>` contains '' as textContent?"
+      (is (= (-> label .-textContent) "Textfield Disabled")))))
+
+(defcard-rg card-textfield-readonly
+  "
+  ## Testing a readonly textfield, see cursor and animation!
+  ```
+  [textfield {:read-only true :value \"Bar\"} \"Textfield ReadOnly\"]
+  ```
+  "
+  [textfield {:read-only true :value "Bar"} "Textfield ReadOnly"])
+
+(deftest test-textfield-readonly
+  (let [label (-> js/document
+                  (.getElementsByClassName "lnz")
+                  (aget 5))
+        name  (-> label .-firstChild)
+        field (-> name .-nextSibling)
+        input (-> field .-firstChild)]
+    (testing "Wrapper is a `<label>`?"
+      (is (= (-> label .-tagName) "LABEL")))
+    (testing "First child of label is an `<i>`?"
+      (is (= (-> name .-tagName) "SPAN")))
+    (testing "Real input is an `<input>`?" 
+      (is (= (-> field .-tagName) "SPAN")))
+    (testing "And input is of type `text`" 
+      (is (= (-> input .-type) "text")))
+    (testing "And input hast value `Foo`" 
+      (is (= (-> input .-value) "Bar")))
+    (testing "`<label>` contains '' as textContent?"
+      (is (= (-> label .-textContent) "Textfield ReadOnly")))))
 
 (defcard-rg card-textfield-icon-without-text
   "
@@ -164,32 +192,30 @@
   ```
   produces in hiccup:
   ```
-  [:label.lnz {:for \"random-uuid\"}                ;; Label wrapper
-    [:i.lnz {:tab-index 0}]                       ;; Stylized Pseudo INPUT
-    [:input {:id \"random-uuid\" :type \"textfield\"}] ;; Real INPUT 
-    [:span [:icon :i.fas.fa-cloud]]]              ;; Icon!
+  [:label.lnz {:for \"random-uuid\"}                          ;; Label wrapper
+    [:span.name \"\"]                                         ;; Field name
+    [:span.field [:input {:type \"text\" :id \"random-uuid\"}]] ;; Real INPUT wrapped by SPAN 
+                 [:i.fas.fa-cloud]]                         ;; Icon
   "
   [textfield {:icon :i.fas.fa-cloud}])
 
 (deftest test-textfield-icon-without-text
   (let [label (-> js/document
                   (.getElementsByClassName "lnz")
-                  (aget 10))
-        pseudo-input (-> label .-firstChild)
-        input (-> pseudo-input .-nextSibling)
-        span  (-> input .-nextSibling)
-        icon  (-> span .-firstChild)]
+                  (aget 6))
+        name  (-> label .-firstChild)
+        field (-> name .-nextSibling)
+        input (-> field .-firstChild)
+        icon  (-> input .-nextSibling)]
     (testing "Wrapper is a `<label>`?"
       (is (= (-> label .-tagName) "LABEL")))
     (testing "First child of label is an `<i>`?"
-      (is (= (-> pseudo-input .-tagName) "I")))
+      (is (= (-> name .-tagName) "SPAN")))
     (testing "Real input is an `<input>`?" 
-      (is (= (-> input .-tagName) "INPUT")))
-    (testing "And input is of type `textfield`" 
-      (is (= (-> input .-type) "textfield")))
-    (testing "`<label>` contains `<span>` as last child"
-      (is (= (-> span .-tagName) "SPAN")))
-    (testing "`<span>` contains an `<i>`"
+      (is (= (-> field .-tagName) "SPAN")))
+    (testing "And input is of type `text`" 
+      (is (= (-> input .-type) "text")))
+    (testing "Icon is an `<i>`?"
       (is (= (-> icon .-tagName) "I")))
     (testing "`<label>` contains '' as textContent?"
       (is (= (-> label .-textContent) "")))))
@@ -207,67 +233,20 @@
 (deftest test-textfield-icon-with-text
   (let [label (-> js/document
                   (.getElementsByClassName "lnz")
-                  (aget 12))
-        pseudo-input (-> label .-firstChild)
-        input (-> pseudo-input .-nextSibling)
-        span  (-> input .-nextSibling)
-        icon  (-> span .-firstChild)]
+                  (aget 7))
+        name  (-> label .-firstChild)
+        field (-> name .-nextSibling)
+        input (-> field .-firstChild)
+        icon  (-> input .-nextSibling)]
     (testing "Wrapper is a `<label>`?"
       (is (= (-> label .-tagName) "LABEL")))
     (testing "First child of label is an `<i>`?"
-      (is (= (-> pseudo-input .-tagName) "I")))
+      (is (= (-> name .-tagName) "SPAN")))
     (testing "Real input is an `<input>`?" 
-      (is (= (-> input .-tagName) "INPUT")))
-    (testing "And input is of type `textfield`" 
-      (is (= (-> input .-type) "textfield")))
-    (testing "`<label>` contains `<span>` as last child"
-      (is (= (-> span .-tagName) "SPAN")))
-    (testing "`<span>` contains an `<i>`"
+      (is (= (-> field .-tagName) "SPAN")))
+    (testing "And input is of type `text`" 
+      (is (= (-> input .-type) "text")))
+    (testing "Icon is an `<i>`?"
       (is (= (-> icon .-tagName) "I")))
-    (testing "`<label>` contains 'cloud' as textContent?"
+    (testing "`<label>` contains 'React!' as textContent?"
       (is (= (-> label .-textContent) "React!")))))
-
-(defcard-rg card-textfield-icon-with-text-and-description
-  "
-  ## With icon, text and description
-  ```
-  [:div
-   [:div 
-    [textfield {:name \"framework\" icon :i.fab.fa-react} 
-     [:span \"React\" [:div [:small \"In computing, React is a JavaScript library for building user interfaces. It is maintained by Facebook, Instagram and a community of individual developers and corporations.\"]]]]]
-   [:div
-    [textfield {:name \"framework\" icon :i.fab.fa-angular}
-     [:span \"Angular\" [:div [:small \"Angular is a TypeScript-based open-source front-end web application platform led by the Angular Team at Google and by a community of individuals and corporations.\"]]]]]]
-  ```
-  "
-  [:div
-   [:div 
-    [textfield {:name "framework" :icon :i.fab.fa-react} 
-     [:span "React" [:div [:small "In computing, React is a JavaScript library for building user interfaces. It is maintained by Facebook, Instagram and a community of individual developers and corporations."]]]]]
-   [:div
-    [textfield {:name "framework" :icon :i.fab.fa-angular}
-     [:span "Angular" [:div [:small "Angular is a TypeScript-based open-source front-end web application platform led by the Angular Team at Google and by a community of individuals and corporations. Angular is a complete rewrite from the same team that built AngularJS."]]]]]])
-
-(deftest test-textfield-icon-with-text-and-description
-  (let [label (-> js/document
-                  (.getElementsByClassName "lnz")
-                  (aget 12))
-        pseudo-input (-> label .-firstChild)
-        input (-> pseudo-input .-nextSibling)
-        span  (-> input .-nextSibling)
-        icon  (-> span .-firstChild)]
-    (testing "Wrapper is a `<label>`?"
-      (is (= (-> label .-tagName) "LABEL")))
-    (testing "First child of label is an `<i>`?"
-      (is (= (-> pseudo-input .-tagName) "I")))
-    (testing "Real input is an `<input>`?" 
-      (is (= (-> input .-tagName) "INPUT")))
-    (testing "And input is of type `textfield`" 
-      (is (= (-> input .-type) "textfield")))
-    (testing "`<label>` contains `<span>` as last child"
-      (is (= (-> span .-tagName) "SPAN")))
-    (testing "`<span>` contains an `<i>`"
-      (is (= (-> icon .-tagName) "I")))
-    (testing "`<label>` contains 'cloud' as textContent?"
-      (is (= (-> label .-textContent) "React!")))))
-
