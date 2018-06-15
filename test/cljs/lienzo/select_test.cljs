7dea02d2538dcaf4eb86c5c0405eb4664cea5599
Daniel Estevez <daniel.ef@gmail.com>
2018-06-14 22:26:19 -0500

    Rewriting select component

--- test/cljs/lienzo/select_test.cljs
+++ test/cljs/lienzo/select_test.cljs
@@ -3,20 +3,12 @@
             [devcards.core :refer-macros [deftest defcard defcard-rg]]
             [sablono.core :as sab]
             [reagent.core :as r]
-            [lienzo.components.select :refer [input-box input-wrapper pseudo-menu mdl-select]]))
+            [lienzo.components.select :refer [select]]))
 
 (defcard
   (str "# Select \n"
        "This namespace defines some functions about select component"))
 
 (defcard-rg test-input-box
-  [input-box])
+  [select])
 
-(defcard-rg test-input-wrapper
-  [input-wrapper])
-
-(defcard-rg test-pseudo-menu
-  [pseudo-menu])
-
-(defcard-rg test-mdl-select
-  [mdl-select])
